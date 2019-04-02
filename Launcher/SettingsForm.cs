605ffd6e94053dcdf52e31eb6b756ff546209429
Sparking <sparking02@yahoo.com.mx>
2019-04-02 13:50:19 -0600

    VS 2019 Update and Clean

--- Launcher/SettingsForm.cs
+++ Launcher/SettingsForm.cs
@@ -1,15 +1,7 @@
-﻿using Launcher.Properties;
-using IniParser;
+﻿using IniParser;
 using IniParser.Model;
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Launcher
